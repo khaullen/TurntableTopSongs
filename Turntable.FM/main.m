7186eba9cf3944ff158e1c40337c7df2ba33936f
Colin Regan <colin@redcup.la>
2013-02-12 17:05:41 -0500

    Initial Commit

---
+++ Turntable.FM/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Turntable.FM
+//
+//  Created by Colin Regan on 2/12/13.
+//  Copyright (c) 2013 Red Cup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
